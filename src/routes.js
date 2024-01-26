8219aa0bfd65fbbcf8a41bfed2ad2c80d413563e
carlos-wylliam <carloswylliam023@gmail.com>
2024-01-26 15:40:13 -0300

    feat: route create user with validate body request and Schema

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const { baseUrl } = require("./controllers/baseUrl")
 const { createUser, login } = require("./controllers/users");
 const { isUserAuthenticated } = require("./middlewares/verifyToken");
+const { validateBodyRequest } = require("./middlewares/validateBodyRequest");
+const { createUserSchema } = require("./lib/userSchema");
 
 const router = Router();
 
 router.get("/", baseUrl)
-router.post("/user", createUser)
+router.post("/user", [validateBodyRequest(createUserSchema)], createUser)
 router.post("/session", login)
 
 router.use(isUserAuthenticated)
