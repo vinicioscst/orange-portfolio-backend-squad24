5bdafea3e0e1457b5f515faaf331657311713acf
carlos-wylliam <carloswylliam023@gmail.com>
2024-01-28 17:36:21 -0300

    chore: table in dump sql

--- dump.sql
+++ dump.sql
@@ -6,3 +6,11 @@ CREATE TABLE users(
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE projects(
+	id SERIAL PRIMARY KEY,
+  title VARCHAR(100),
+  tags VARCHAR(50),
+  link VARCHAR(255),
+  description VARCHAR(255)
+);
