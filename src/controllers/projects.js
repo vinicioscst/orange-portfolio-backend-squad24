2511365b23020c9061cc28bf6bcbf03fbdb0fbd4
carlos-wylliam <carloswylliam023@gmail.com>
2024-01-29 15:36:32 -0300

    fix: insert user id in the projects table

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -2,16 +2,17 @@ const { pool } = require("../lib/postgres");
 
 const createProject = async (req, res) => {
   const { title, tags, link, description } = req.body;
+  const userId = req.user.id;
 
   try {
-    const params = [title, tags, link, description];
+    const params = [title, tags, link, description, userId ];
 
     const createdProject = await pool.query(
       `
               INSERT INTO projects
-              (title, tags, link, description)
+              (title, tags, link, description, user_id)
               VALUES
-              ($1, $2, $3, $4)
+              ($1, $2, $3, $4, $5)
               RETURNING *
             `,
       params
