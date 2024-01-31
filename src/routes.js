ba3a75bddf55846aa581e5fb7eb04c166fbc577b
carlos-wylliam <carloswylliam023@gmail.com>
2024-01-31 17:39:51 -0300

    feat: endpoint upload images

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,11 @@
 const { Router } = require("express");
 const { baseUrl } = require("./controllers/baseUrl");
-const { createUser, login, getUsers, googleLogin } = require("./controllers/users");
+const {
+  createUser,
+  login,
+  getUsers,
+  googleLogin,
+} = require("./controllers/users");
 const { isUserAuthenticated } = require("./middlewares/verifyToken");
 const { validateBodyRequest } = require("./middlewares/validateBodyRequest");
 const { createUserSchema, userLoginSchema } = require("./lib/userSchema");
@@ -10,13 +15,15 @@ const {
   getUserProject,
 } = require("./controllers/projects");
 const { createProjectSchema } = require("./lib/projectSchema");
+const multer = require("./lib/multer");
+const { uploadImages } = require("./controllers/upload");
 
 const router = Router();
 
 router.get("/", baseUrl);
 router.post("/user", [validateBodyRequest(createUserSchema)], createUser);
 router.post("/session", [validateBodyRequest(userLoginSchema)], login);
-router.post("/session/google", googleLogin)
+router.post("/session/google", googleLogin);
 router.get("/user", getUsers);
 
 router.use(isUserAuthenticated);
@@ -27,6 +34,7 @@ router.post(
 );
 router.get("/projects", getProjects);
 router.get("/projects/user", getUserProject);
+router.post("/projects/upload", [multer.single("file")], uploadImages);
 module.exports = {
   router,
 };
