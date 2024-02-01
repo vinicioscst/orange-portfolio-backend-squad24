46d332d8f9398fb5d63a5589ed8207ee39fcbb53
carlos-wylliam <carloswylliam023@gmail.com>
2024-02-01 19:26:46 -0300

    fix: config cors

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors({
-  origin: '*',
+  origin: 'http://localhost:5173',
   credentials: true,
-  optionSucessStatus: 200
-}))
+  optionSuccessStatus: 200,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+
 app.use(express.json());
 app.use(router);
 
