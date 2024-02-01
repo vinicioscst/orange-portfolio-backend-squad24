236f4586433175817bc9475d4235ca7ce5d41e2d
Mariana Moreira <moreiramarimana137@gmail.com>
2024-02-01 19:39:04 -0300

    feat: add contrtoller to update project

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -48,8 +48,74 @@ const getUserProject = async (req, res) => {
     return res.status(500).json({ mensagem: "Erro interno do servidor" });
   }
 };
+
+const updateProject = async (req, res) => {
+  const { id: project_id } = req.params;
+  const { title, tags, link, description, createdDate, userId } = req.body;
+  const { mimetype, originalname, buffer } = req.file || {
+    mimetype: null,
+    originalname: null,
+    buffer: null,
+  };
+
+  try {
+    let project = await pool.query("SELECT * FROM projects WHERE id = $1", [
+      project_id,
+    ]);
+
+    if (project.rowCount === 0) {
+      return res
+        .status(404)
+        .json({ mensagem: "O Projeto informado não foi encontrado" });
+    }
+
+    const imageUrl = product.rows[0].image;
+
+    if (imageUrl) {
+      const lastSlashIndex = imageUrl.lastIndexOf("/");
+      const fileName = imageUrl.substring(lastSlashIndex + 1);
+
+      await deleteImage(`projects/${project_id}/${fileName}`);
+    }
+
+    if (originalname) {
+      const image = await uploadImage(
+        `projects/${project_id}/${originalname}`,
+        buffer,
+        mimetype
+      );
+
+      const projectWithImage = await pool.query(
+        "UPDATE projects SET title = $1, tags = $2, link = $3, description = $4, createdDate = $5, userId = $6, image = $7 WHERE id = $8 RETURNING *",
+        [
+          title,
+          tags,
+          link,
+          description,
+          createdDate,
+          userId,
+          image.url,
+          project_id,
+        ]
+      );
+
+      return res.status(200).json(projectWithImage.rows[0]);
+    }
+    project = await pool.query(
+      "UPDATE projects SET title = $1, tags = $2, link = $3, description = $4, createdDate = $5, userId = $6, image = $7 WHERE id = $8 RETURNING *",
+      [title, tags, link, description, createdDate, userId, null, project_id]
+    );
+
+    return res.status(200).send(project.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ mensagem: "Erro interno do servidor." });
+  }
+};
+
 module.exports = {
   createProject,
   getProjects,
   getUserProject,
+  updateProject,
 };
